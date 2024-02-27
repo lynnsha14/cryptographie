14dced148f41f2e86629412355b4f838b5c017bf
lynnsha14 <52491078+lynnsha14@users.noreply.github.com>
2024-02-28 00:45:38 +0100

    Add files via upload

--- Cesar.java
+++ Cesar.java
@@ -1,4 +1,14 @@
 
-public class Cesar {
+public class Cesar extends SubstitutionType {
+	
+	
+	public Cesar {}
+	public String encrypt(String plaintext) {
+		return plaintext;}
+	public String decrypt(String ciphertext) {
+		return ciphertext;}
+
+	public String toString() {
+		return key;}
 
 }
